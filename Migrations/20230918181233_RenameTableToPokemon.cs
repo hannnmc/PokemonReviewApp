c98bb84dafd5808809444a629f0731f78cc3cebb
Han Chen <hanmikechen@gmail.com>
2023-09-18 19:22:36 -0400

    create interface, controllers, and repository pattern

---
+++ Migrations/20230918181233_RenameTableToPokemon.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PokemonReviewApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameTableToPokemon : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PokemonCategories_Pokemons_PokemonId",
+                table: "PokemonCategories");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_PokemonOwners_Pokemons_PokemonId",
+                table: "PokemonOwners");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reviews_Pokemons_PokemonId",
+                table: "Reviews");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Pokemons",
+                table: "Pokemons");
+
+            migrationBuilder.RenameTable(
+                name: "Pokemons",
+                newName: "Pokemon");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Pokemon",
+                table: "Pokemon",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PokemonCategories_Pokemon_PokemonId",
+                table: "PokemonCategories",
+                column: "PokemonId",
+                principalTable: "Pokemon",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PokemonOwners_Pokemon_PokemonId",
+                table: "PokemonOwners",
+                column: "PokemonId",
+                principalTable: "Pokemon",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reviews_Pokemon_PokemonId",
+                table: "Reviews",
+                column: "PokemonId",
+                principalTable: "Pokemon",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PokemonCategories_Pokemon_PokemonId",
+                table: "PokemonCategories");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_PokemonOwners_Pokemon_PokemonId",
+                table: "PokemonOwners");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reviews_Pokemon_PokemonId",
+                table: "Reviews");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Pokemon",
+                table: "Pokemon");
+
+            migrationBuilder.RenameTable(
+                name: "Pokemon",
+                newName: "Pokemons");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Pokemons",
+                table: "Pokemons",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PokemonCategories_Pokemons_PokemonId",
+                table: "PokemonCategories",
+                column: "PokemonId",
+                principalTable: "Pokemons",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PokemonOwners_Pokemons_PokemonId",
+                table: "PokemonOwners",
+                column: "PokemonId",
+                principalTable: "Pokemons",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reviews_Pokemons_PokemonId",
+                table: "Reviews",
+                column: "PokemonId",
+                principalTable: "Pokemons",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
